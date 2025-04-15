f5765e564b5c3d706d9c668202c0d2b1aa82b669
Sachin Gupta <sachin.gupta@peason.com>
2025-04-15 16:38:44 +0530

    Initial-Nodemon Route and route handler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("Hey")
+
+const express = require("express");
+const app = express();
+// app.use((req,res)=>{
+//     res.send("Hello World")
+// })
+// app.use('/',(req,res)=>{
+//     res.send("Hello World from root")
+// })
+app.use('/hello',(req,res)=>{
+    res.send("Hello World from hello")
+})
+
+app.use('/test',(req,res)=>{
+    res.send("Hello World from test")
+})
+
+app.listen(8080,()=>{
+    console.log("Server Running at Port : 8080......");
+})
