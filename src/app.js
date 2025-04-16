2e653978b606018d40c05176060405fd4cd3100a
Sachin Gupta <sachin.gupta@peason.com>
2025-04-17 00:13:07 +0530

    Feed API & User by Email API

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   const userObject = {
-   ...req.body
+    ...req.body,
   };
   const user = new User(userObject); // creating a new instance of user model for this data
   try {
@@ -16,6 +16,35 @@ app.post("/signup", async (req, res) => {
     return res.status(400).send({ message: "Internal Server Error" });
   }
 });
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  try {
+    const emailId = req.body.emailId
+    const users = await User.find({ emailId: emailId });
+    if (!users || users.length===0) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    res.status(200).send(users); // sending the user object as response to the ap
+  } catch (err) {
+    console.log("err:::", err);
+    res.status(400).send({ message: "Something went wrong" });
+  }
+});
+
+// feed api [GET]- all users from the DB
+app.get("/feed", async (req, res) => {
+    try {
+      const users = await User.find({}); // with {} => all users from the collection will get
+      if (!users || users.length===0) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      res.status(200).send(users); // sending the user object as response to the ap
+    } catch (err) {
+      console.log("err:::", err);
+      res.status(400).send({ message: "Something went wrong" });
+    }
+  });
 
 app.use("/", (err, req, res, next) => {
   console.log("error:::", err);
