5e70ed976d2a6d02882e04d5379b2ffb47243728
Sachin Gupta <sachin.gupta@peason.com>
2025-04-16 18:19:41 +0530

    SINGUP API - WIP

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const { connectDB } = require("../src/config/database");
+const User = require("./models/user");
 const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const userObject = {
+   ...req.body
+  };
+  const user = new User(userObject); // creating a new instance of user model for this data
+  try {
+    await user.save();
+    res.send({ message: "User Created" });
+  } catch (err) {
+    return res.status(400).send({ message: "Internal Server Error" });
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  console.log("error:::", err);
+  res.status(500).send({ message: "Internal Server Error" });
+});
 
 connectDB()
   .then((res) => {
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uri =
-  "mongodb+srv://sachingupta769:kfOXK9o2iukT1WZF@namastenode-dev.6ctfxf3.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode-Dev";
+  "mongodb+srv://sachingupta769:kfOXK9o2iukT1WZF@namastenode-dev.6ctfxf3.mongodb.net/Learning?retryWrites=true&w=majority&appName=NamasteNode-Dev";
 
 const connectDB = async () => await mongoose.connect(uri);
 module.exports = { connectDB };
