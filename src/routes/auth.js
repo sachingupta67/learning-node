f6b360fb0564723160cf10310a5552b0b6797ef2
Sachin Gupta <sachin.gupta@peason.com>
2025-04-18 00:18:36 +0530

    Logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,34 +29,40 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-      if (!emailId || !password) {
-        throw new Error("EmailId and Password are required");
-      } else if (!validator.isEmail(emailId)) {
-        throw new Error("Invalid EmailId");
-      }
-      const user = await User.findOne({ emailId: emailId });
-      if (!user) {
-        throw new Error("Credentials are not valid");
-      }
-      const isPasswordValid = await user.validatePassword(password); // offload the logic to user model
-      if (isPasswordValid) {
-        // Create a JWT token and send it in the response
-        const token = await user.getJWT();
-        // add cookie
-        res.cookie("token", token, {
-          expires: new Date(Date.now() + 8 * 3600000), // 1 day
-          // secure: true, // work with https only
-          // httpOnly: true, // work with http only
-        });
-        res.send({ message: "Login Successful", data: user });
-      } else {
-        throw new Error("Credentials are not valid");
-      }
-    } catch (err) {
-      res.status(500).send({ message: err.message });
+  try {
+    const { emailId, password } = req.body;
+    if (!emailId || !password) {
+      throw new Error("EmailId and Password are required");
+    } else if (!validator.isEmail(emailId)) {
+      throw new Error("Invalid EmailId");
     }
-  });
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Credentials are not valid");
+    }
+    const isPasswordValid = await user.validatePassword(password); // offload the logic to user model
+    if (isPasswordValid) {
+      // Create a JWT token and send it in the response
+      const token = await user.getJWT();
+      // add cookie
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000), // 1 day
+        // secure: true, // work with https only
+        // httpOnly: true, // work with http only
+      });
+      res.send({ message: "Login Successful", data: user });
+    } else {
+      throw new Error("Credentials are not valid");
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
 
+authRouter.post("/logout", (_req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send({ message: "Logout Successful" });
+});
 module.exports = authRouter;
