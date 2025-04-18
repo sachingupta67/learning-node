6f68b9e7ded5241b212803d19085817582f6f4f6
Sachin Gupta <sachin.gupta@peason.com>
2025-04-18 18:11:19 +0530

    Table Connection - Ref & Populate

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -5,6 +5,7 @@ const connectionRequestSchema = new mongoose.Schema(
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref:'User' // reference to the user collection here 'User' is model name
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
