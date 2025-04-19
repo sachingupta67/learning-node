956f246ebda08b315418237d307d3e4a97e77b24
Sachin Gupta <sachin.gupta@peason.com>
2025-04-19 21:57:38 +0530

    prettier change

--- src/app.js
+++ src/app.js
@@ -9,17 +9,19 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, 
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/",authRouter);
-app.use("/",profileRouter);
-app.use("/",requestRouter);
-app.use('/',userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 app.use("/", (err, req, res, next) => {
   res.status(500).send({ message: err.message });
