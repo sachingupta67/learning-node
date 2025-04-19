8e290a67e17fac6702caed18d7d174f92d14ff1a
Sachin Gupta <sachin.gupta@peason.com>
2025-04-19 21:28:47 +0530

    WhiteListing frontend

--- src/app.js
+++ src/app.js
@@ -9,7 +9,10 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
-app.use(cors())
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+}))
 app.use(express.json());
 app.use(cookieParser());
 
